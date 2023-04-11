79b37167f95d8feb0d68a10c972294605dc7e811
Xiaoyuan Lu <xyuanlu@gmail.com>
2023-04-10 23:27:30 -0700

    2

--- zookeeper-api/src/test/java/org/apache/helix/zookeeper/impl/client/TestRawZkClient.java
+++ zookeeper-api/src/test/java/org/apache/helix/zookeeper/impl/client/TestRawZkClient.java
@@ -549,6 +549,7 @@ public class TestRawZkClient extends ZkTestBase {
     });
     lock.lock();
 
+
     _zkClient.writeData(TEST_PATH, "Test");
     Assert.assertTrue(callbackFinish.await(10, TimeUnit.SECONDS));
     verifyResult = TestHelper.verify(() -> { return
